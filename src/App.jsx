e2fcc97a63e0612d74ff8fa36969032a189a5c39
Caleb-Rogers <rogerscaleb2019@gmail.com>
2023-01-11 14:12:20 -0500

    Organization & Resumed Progress
    Adapted HTML, CSS, and images from main branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <>
+      <header class="header">
+        <h3 class="logo-nav">B. Monroe Consulting</h3>
+        <nav class="main-nav">
+          <ul class="main-nav-list">
+            <li><a class="main-nav-link" href="#">Section 1</a></li>
+            <li><a class="main-nav-link" href="#">Section 2</a></li>
+            <li><a class="main-nav-link" href="#">Section 3</a></li>
+            <li><a class="main-nav-link" href="#">Section 4</a></li>
+            <li><a class="main-nav-link nav-cta" href="#">Section 5</a></li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <section class="section-hero">
+          <div class="hero">
+            <div class="hero-text-box">
+              <h1 class="heading-primary">
+                We handle the logistics of your construction project so you can focus on what's important
+              </h1>
+              <p class="hero-description">
+                Receive professional expertise on project management and delegate your legal work such as processing building permits, verifying building code & regulations, and performing safety & efficiency tests
+              </p>
+              <a href="#" class="btn btn--full margin-right-sm">CONTACT</a>
+              <a href="#" class="btn btn--outline">LEARN MORE &darr;</a>
+            </div>
+            <div class="hero-img-box">
+              <img src="/src/assets/hero.svg" class="hero-img" alt="Working Woman" />
+            </div>
+          </div>
+        </section>
+      </main>
+    </>
   )
 }
 
