f993e2571d1190b212d37ddd7707882b17096451
Caleb-Rogers <rogerscaleb2019@gmail.com>
2023-01-11 13:28:35 -0500

    Adding Tailwind preset screen sizes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,8 +10,16 @@ module.exports = {
         tintDark: "#459ade",
       },
       fontFamily: {
-        aleo: ["Aleo", "sans-serif"]
+        aleo: ["Aleo", "sans-serif"],
       },
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
     },
   },
   plugins: [],
