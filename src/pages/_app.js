55f251a5f227f76febf63e4fcf9abf2792a130c1
KyleRessLiere <kressliere@gmail.com>
2023-01-14 12:39:58 -0500

    added boostrap

---
+++ src/pages/_app.js
@@ -0,0 +1,20 @@
+import Head from "next/head";
+import "@/styles/globals.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+<link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+  integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
+  crossorigin="anonymous"
+/>;
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
+export default MyApp;
