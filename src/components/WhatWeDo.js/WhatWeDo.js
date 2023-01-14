8c779abaacec017716f824f3185d932b11b7752e
KyleRessLiere <kressliere@gmail.com>
2023-01-14 17:29:56 -0500

    fixed captioning text size for whatWeDo page

--- src/components/WhatWeDo.js/WhatWeDo.js
+++ src/components/WhatWeDo.js/WhatWeDo.js
@@ -15,7 +15,7 @@ function WhatWeDo() {
             alt="First slide"
           />
           <Carousel.Caption>
-            <h3>First slide label</h3>
+            <h1 className="caption-font">First slide label</h1>
             <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
           </Carousel.Caption>
         </Carousel.Item>
@@ -27,7 +27,7 @@ function WhatWeDo() {
           />
 
           <Carousel.Caption className="carousel-caption">
-            <h3>Second slide label</h3>
+            <h1 className="caption-font">Second slide label</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
           </Carousel.Caption>
         </Carousel.Item>
@@ -39,7 +39,7 @@ function WhatWeDo() {
           />
 
           <Carousel.Caption>
-            <h3>Third slide label</h3>
+            <h1 className="caption-font">Third slide label</h1>
             <p>
               Praesent commodo cursus magna, vel scelerisque nisl consectetur.
             </p>
