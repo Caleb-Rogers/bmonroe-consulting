a5a1b2a33858c4086ea4784db89e79f13e97996a
Caleb-Rogers <rogerscaleb2019@gmail.com>
2023-01-18 21:08:07 -0500

    Services images & content

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -7,34 +7,34 @@ import { Wrench } from "phosphor-react";
 function Services() {
   const cardInfo = [
     {
-      image: "https://i.insider.com/50f967f56bb3f7830a000019",
+      image: "../public/research-icon.png",
       title: "Zoning, Land Use, & Research",
-      text: "Lorem ipsum dolor sit ametincididvenexin reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      text: "Zoning and land use research, Location analysis, Site plan approvals, Conditional use permits, Zoning ordinance appeals, Zoning ordinance variances, Zoning ordinance amending, Property re-zoning, Architectural review approvals, Public Works and Transportation Approvals, Zoning violations.",
     },
     {
       image: "https://i.insider.com/50f967f56bb3f7830a000019",
-      title: "Lebron James2",
-      text: "Lorem ipsum dolor sit ametincididvenexin reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      title: "Code Enforcement",
+      text: "Demolition permits, Building Codes Analysis, Building Permits, Occupancy permits, Special Permits, Temporary Structures permits, Code violations",
     },
     {
       image: "https://i.insider.com/50f967f56bb3f7830a000019",
-      title: "Lebron James3",
-      text: "Lorem ipsum dolor sit ametincididvenexin reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      title: "Design",
+      text: "Pre-design work to ensure compliance with the Code, Site plan design, Pre-design work for engineers and architects",
     },
     {
       image: "https://i.insider.com/50f967f56bb3f7830a000019",
-      title: "Lebron James4",
-      text: "Lorem ipsum dolor sit ametincididvenexin reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      title: "Environment Permitting & Meeting Representation",
+      text: "Architectural Review Boards, Historic District Commissions, Planning Commissions, Town Boards, Zoning Board of Appeals, Building Code Board of Appeals, City and County Councils, State Regulatory Agencies",
     },
     {
       image: "https://i.insider.com/50f967f56bb3f7830a000019",
-      title: "Lebron James5",
-      text: "Lorem ipsum dolor sit ametincididvenexin reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      title: "Energy Analysis",
+      text: "Certified BPI Infiltration and Duct Leakage(Blower Door Testing), Home efficiency analysis",
     },
     {
       image: "https://i.insider.com/50f967f56bb3f7830a000019",
-      title: "Lebron James6",
-      text: "Lorem ipsum dolor sit ametincididvenexin reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      title: "Land management",
+      text: "Analyzing feasibility for the highest and best use for the asset or land, Researching property history and allowable uses, Coordinating permitting, Planning the scope, phasing, and project delivery method, Managing properties for absentee landowners",
     },
   ];
   const renderCard = (card, index) => {
