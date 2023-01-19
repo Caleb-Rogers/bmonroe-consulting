a41a251d47a17ac3acecd146846c519720bdd98a
KyleRessLiere <kressliere@gmail.com>
2023-01-18 19:10:21 -0500

    service font adjustments

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -39,22 +39,24 @@ function Services() {
   ];
   const renderCard = (card, index) => {
     return (
-      <Col sm={3} className="mb-4 service-col   ">
-        <Card className="serviceCard h-100 w-100">
+      <Col sm={3} className="mb-4 service-col p-3  ">
+        <Card className="service-card h-100 w-100">
           <Card.Img variant="top" src="holder.js/100px160" />
           <Card.Body>
-            <Card.Title>{card.title}</Card.Title>
-            <Card.Text>asdasdas</Card.Text>
+            <Card.Title className="service-title">{card.title}</Card.Title>
+            <Card.Text className="service-desc lead">asdasdas</Card.Text>
           </Card.Body>
         </Card>
       </Col>
     );
   };
+
   return (
     <Container>
       <h1 className="text-center sectionHeadingBlue pt-4  ">Services</h1>
       <hr></hr>
-      <Row md={3} className="p-8">
+
+      <Row md={3} className="p-8 w-100">
         {cardInfo.map(renderCard)}
       </Row>
     </Container>
