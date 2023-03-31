fab490fbc4127734cc9e4b3c08f71af304704c17
Caleb-Rogers <rogerscaleb2019@gmail.com>
2023-03-31 14:00:14 -0400

    AboutMe section redesign
    - image on left: circle design
    - text on right: heading, horizontal line, description, icons

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -2,30 +2,22 @@ import React from "react";
 
 const AboutMe = () => {
   return (
-    <div className="about-container ">
-      <div className="about-text-box">
-        <h1 className="about-heading">I'm Barb.</h1>
-        <h2 className="about-subheading">
+    <div class="about-container">
+      <div class="about-img-box">
+        <img src="headshot.png" class="about-img" alt="B. Monroe Headshot" />
+      </div>
+      <div class="about-text-box">
+        <h2 class="about-heading">ABOUT BARB</h2>
+        <hr class="about-line" />
+        <h3 class="about-subheading">
           I help designers and engineers through the legal process of getting
           their construction project approved, as well as offering expertise on
           site plans and project management.
-        </h2>
-        <ul className="about-list">
-          <li className="about-list-item">
-            NYS Licensed Code Enforcement Official
-          </li>
-          <li className="about-list-item">20+ years as a ZEO</li>
-          <li className="about-list-item">
-            Building Performance Institute, Inc.: IDL Certification
-          </li>
-        </ul>
-      </div>
-      <div className="about-img-box">
-        <img
-          src="headshot.png"
-          className="about-img"
-          alt="B. Monroe Headshot"
-        />
+        </h3>
+        <div class="about-icons">
+          <img src="icon1.png" alt="Icon 1" class="about-icon" />
+          <img src="icon2.png" alt="Icon 2" class="about-icon" />
+        </div>
       </div>
     </div>
   );
