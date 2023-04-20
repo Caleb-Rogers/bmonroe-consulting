d950d64a8b3a5aa9b67ffedf652fb464f3fa5405
KyleRessLiere <kressliere@gmail.com>
2023-04-20 12:11:40 -0400

    email not fills based on form

--- src/components/ContactMe/ContactMe.js
+++ src/components/ContactMe/ContactMe.js
@@ -1,6 +1,21 @@
 function ContactMe() {
+  function handleSubmit(event) {
+    event.preventDefault();
+    const { name, email, phone, message } = event.target.elements;
+    const subject = `Contact Form: ${name.value} - ${message.value.substring(
+      0,
+      50
+    )}`;
+    const body = `Name: ${name.value}\nEmail: ${email.value}\nPhone: ${phone.value}\n\nMessage: ${message.value}`;
+    const mailtoUrl = `mailto:contact@example.com?subject=${encodeURIComponent(
+      subject
+    )}&body=${encodeURIComponent(body)}`;
+    window.location.href = mailtoUrl;
+  }
+
   return (
     <footer className="footer-distributed">
+      {/* ... */}
       <div className="footer-left">
         <h3>B. Monroe Consulting</h3>
 
@@ -16,15 +31,13 @@ function ContactMe() {
           </a>
         </div>
       </div>
-
       <div className="footer-right">
         <p>Contact Us</p>
-
-        <form action="#" method="post">
-          <input type="text" name="name" placeholder="Name" />
-          <input type="text" name="email" placeholder="Email" />
-          <input type="text" name="phone" placeholder="Phone Number" />
-          <textarea name="message" placeholder="Message"></textarea>
+        <form onSubmit={handleSubmit}>
+          <input type="text" name="name" placeholder="Name" required />
+          <input type="email" name="email" placeholder="Email" required />
+          <input type="tel" name="phone" placeholder="Phone Number" required />
+          <textarea name="message" placeholder="Message" required></textarea>
           <button type="submit">Send</button>
         </form>
       </div>
