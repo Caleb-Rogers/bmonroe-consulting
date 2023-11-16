38db6e38d46783dbb58d4a22b78be437b187c3f2
Caleb-Rogers <rogerscaleb2019@gmail.com>
2023-11-16 18:40:14 -0500

    About Blur Animation
    Blur animation works on scroll. "prefers-reduced-motion" not triggering when scrolling back up (unless expected?). Also not transforming from right. Still needs tweaking...

--- src/components/About/AboutPage.js
+++ src/components/About/AboutPage.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { useEffect } from "react";
 
 <>
 <link rel="preconnect" href="https://fonts.googleapis.com"></link>
@@ -7,6 +8,41 @@ import Link from "next/link";
 </>
 
 export default function About() {
+    useEffect(() => {
+        if ("IntersectionObserver" in window) {
+          const observer = new IntersectionObserver((entries) => {
+            entries.forEach((entry) => {
+              console.log(entry);
+              if (entry.isIntersecting) {
+                entry.target.classList.add('show');
+              } else {
+                entry.target.classList.remove('show');
+              }
+            });
+          });
+
+          const hiddenElements = document.querySelectorAll('.hidden');
+          hiddenElements.forEach((element) => observer.observe(element));
+
+          return () => {
+            hiddenElements.forEach((element) => observer.unobserve(element));
+          };
+        }
+      }, []);
+      /*
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            console.log(entry);
+            if (entry.isIntersecting) {
+                entry.target.classList.add('show');
+            } else {
+                entry.target.classList.remove('show');
+            }
+        });
+    });
+    const hiddenElements = document.querySelectorAll('.hidden');
+    hiddenElements.forEach((element) => observer.observe(element));
+    */
     return (
       <>
         <div className="about-container">
@@ -25,73 +61,85 @@ export default function About() {
                     <br></br>
                     <br></br>
                     <br></br>
-                    <h2>WORK THAT I'M PROUD OF</h2>
-                    <br></br>
-                    <p>Mostly:</p>
-                    <p>I was the lead project manager for <a href="https://www.redpointny.com/">Red Point</a>. During my
-                        time in that role I designed site plans and processed permits to get approved, allowing the business
-                        to execute on their construction project. I cultivated meaningful relationships with with my clients
-                        and am regarded as a efficient and effective member that can improve any team I join.
-                    </p>
-                    <br></br>
-                    <p>But also:</p>
-                    <ul>
-                        <li>Built and structured the Construction Project Manager Consultant role from the ground up</li>
-                        <li>Outlined preliminary structure for facilitating streamlined and efficient communication between internal teams</li>
-                        <li>Shifted primary focus of ongoing partnerships from a task based structure to strategy based structure</li>
-                        <li>Created customized strategic planning recommendations matching clients exact needs</li>
-                        <li>Conducted creative, strategic planning, and problem solving sessions with Red Point and client teams</li>
-                        <li>Translated company capabilities into formal client strategic planning, proposal and pitch documentation</li>
-                        <li>Served as client archive and record for case study development and relationship engagement</li>
-                        <li>Maintained and increased healthy client relationships</li>
-                    </ul>
-                    <br></br>
-                    <br></br>
-                    <br></br>
-                    <h2>FROM THE CAREER ARCHIVES</h2>
-                    <br></br>
-                    <h3>Construction Project Management Consultant, Red Point | 2019 - 2023</h3>
-                    <p>Maintained, developed, increased client partnerships by cultivating close relationships, built on trust and shared vision.</p>
-                    <br></br>
-                    <h3>Site Plan Designer, Other Company | 2018-2022</h3>
-                    <p>Managed multiple large scale redesign projects (averaging 12 months) and general ongoing support clients simultaneously</p>
-                    <br></br>
-                    <br></br>
-                    <br></br>
-                    <h2>NOTEWORTHY CLIENT RELATIONSHIPS AS CONSTRUCTION PROJECT MANAGER CONSULTANT</h2>
-                    <br></br>
-                    <div><a href="https://www.redpointny.com/">Client 1</a></div>
-                    <div><a href="https://www.redpointny.com/">Client 2</a></div>
-                    <div><a href="https://www.redpointny.com/">Client 3</a></div>
-                    <div><a href="https://www.redpointny.com/">Client 4</a></div>
-                    <div><a href="https://www.redpointny.com/">Client 5</a></div>
-                    <br></br>
-                    <br></br>
-                    <h2>PROFESSIONAL DEVELOPMENT</h2>
-                    <br></br>
-                    <h3>Positioning Your Agency Workshop</h3>
-                    <p>Bureau of Digital | September 2022</p>
-                    <br></br>
-                    <h3>Biz Dev Workshop</h3>
-                    <p>Bureau of Digital | June 2022</p>
-                    <br></br>
-                    <h3>Account Management Camp</h3>
-                    <p>Bureau of Digital | May 2022</p>
-                    <br></br>
-                    <h3>Account Management Camp</h3>
-                    <p>Bureau of Digital | May 2021</p>
-                    <br></br>
-                    <br></br>
-                    <br></br>
-                    <h2>ABOUT ME</h2>
-                    <br></br>
-                    <h3>Family movie nights</h3>
-                    <h3>Draft Beer</h3>
-                    <h3>Dinner and drinks</h3>
-                    <h3>Amy Schumer</h3>
-                    <h3>Daptone Records</h3>
-                    <h3>Pasta</h3>
-                    <h3>Sunday hangs</h3>
+                    <div className="hidden">
+                        <h2>WORK THAT I'M PROUD OF</h2>
+                        <br></br>
+                        <p>Mostly:</p>
+                        <p>I was the lead project manager for <a href="https://www.redpointny.com/">Red Point</a>. During my
+                            time in that role I designed site plans and processed permits to get approved, allowing the business
+                            to execute on their construction project. I cultivated meaningful relationships with with my clients
+                            and am regarded as a efficient and effective member that can improve any team I join.
+                        </p>
+                        <br></br>
+                    </div>
+                    <div className="hidden">
+                        <p>But also:</p>
+                        <ul>
+                            <li>Built and structured the Construction Project Manager Consultant role from the ground up</li>
+                            <li>Outlined preliminary structure for facilitating streamlined and efficient communication between internal teams</li>
+                            <li>Shifted primary focus of ongoing partnerships from a task based structure to strategy based structure</li>
+                            <li>Created customized strategic planning recommendations matching clients exact needs</li>
+                            <li>Conducted creative, strategic planning, and problem solving sessions with Red Point and client teams</li>
+                            <li>Translated company capabilities into formal client strategic planning, proposal and pitch documentation</li>
+                            <li>Served as client archive and record for case study development and relationship engagement</li>
+                            <li>Maintained and increased healthy client relationships</li>
+                        </ul>
+                        <br></br>
+                        <br></br>
+                        <br></br>
+                    </div>
+                    <div className="hidden">
+                        <h2>FROM THE CAREER ARCHIVES</h2>
+                        <br></br>
+                        <h3>Construction Project Management Consultant, Red Point | 2019 - 2023</h3>
+                        <p>Maintained, developed, increased client partnerships by cultivating close relationships, built on trust and shared vision.</p>
+                        <br></br>
+                        <h3>Site Plan Designer, Other Company | 2018-2022</h3>
+                        <p>Managed multiple large scale redesign projects (averaging 12 months) and general ongoing support clients simultaneously</p>
+                        <br></br>
+                        <br></br>
+                        <br></br>
+                    </div>
+                    <div className="hidden">
+                        <h2>NOTEWORTHY CLIENT RELATIONSHIPS AS CONSTRUCTION PROJECT MANAGER CONSULTANT</h2>
+                        <br></br>
+                        <div><a href="https://www.redpointny.com/">Client 1</a></div>
+                        <div><a href="https://www.redpointny.com/">Client 2</a></div>
+                        <div><a href="https://www.redpointny.com/">Client 3</a></div>
+                        <div><a href="https://www.redpointny.com/">Client 4</a></div>
+                        <div><a href="https://www.redpointny.com/">Client 5</a></div>
+                        <br></br>
+                        <br></br>
+                    </div>
+                    <div className="hidden">
+                        <h2>PROFESSIONAL DEVELOPMENT</h2>
+                        <br></br>
+                        <h3>Positioning Your Agency Workshop</h3>
+                        <p>Bureau of Digital | September 2022</p>
+                        <br></br>
+                        <h3>Biz Dev Workshop</h3>
+                        <p>Bureau of Digital | June 2022</p>
+                        <br></br>
+                        <h3>Account Management Camp</h3>
+                        <p>Bureau of Digital | May 2022</p>
+                        <br></br>
+                        <h3>Account Management Camp</h3>
+                        <p>Bureau of Digital | May 2021</p>
+                        <br></br>
+                        <br></br>
+                        <br></br>
+                    </div>
+                    <div className="hidden">
+                        <h2>ABOUT ME</h2>
+                        <br></br>
+                        <h3>Family movie nights</h3>
+                        <h3>Draft Beer</h3>
+                        <h3>Dinner and drinks</h3>
+                        <h3>Amy Schumer</h3>
+                        <h3>Daptone Records</h3>
+                        <h3>Pasta</h3>
+                        <h3>Sunday hangs</h3>
+                    </div>
                 </div>
                 <div className="about-main-right">
                     <img className="headshot" src="headshot.png" alt="B. Monroe Headshot" />
