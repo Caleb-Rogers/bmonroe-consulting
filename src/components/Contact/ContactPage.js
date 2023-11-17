a700a2bc2735fb6428e3eebe5b66a8e14b231f48
Caleb-Rogers <rogerscaleb2019@gmail.com>
2023-11-17 03:08:49 -0500

    Contact Form Styling
    The label translate not yet working...

--- src/components/Contact/ContactPage.js
+++ src/components/Contact/ContactPage.js
@@ -13,40 +13,40 @@ export default function Contact() {
                     <div className="box">
                         <div className="icon"><img src="address-100-yellow.png" alt="Address Icon" /></div>                        <div className="text">
                             <h3>Address</h3>
-                            <p>1234 Cty Hwy,<br></br>Laurens, NY,<br></br>13796</p>
+                            <p>1234 Cty Hwy<br></br>Laurens, NY<br></br>13796</p>
                         </div>
                     </div>
                     <div className="box">
                         <div className="icon"><img src="phone-500-yellow.png" alt="Address Icon" /></div>
                         <div className="text">
                             <h3>Phone</h3>
-                            <p>123-456-7890</p>
+                            <p>(607) 267-7687</p>
                         </div>
                     </div>
                     <div className="box">
                         <div className="icon"><img src="email-64-yellow.png" alt="Address Icon" /></div>
                         <div className="text">
                             <h3>Email</h3>
-                            <p>bmonroe@gmail.com</p>
+                            <p>milfordnyzeo@gmail.com</p>
                         </div>
                     </div>
                 </div>
                 <div className="contact-form">
                     <form method="POST" action="https://www.random.com/">
-                        <h2>Send Message</h2>
+                        <h2>Send Us An Email</h2>
                         <div className="input-field">
                             <label>Full Name</label>
-                            <input type="text" name="name" />
+                            <input type="text" name="name" required />
                         </div>
                         <div className="input-field">
                             <label>Email</label>
-                            <input type="text" name="email" />
+                            <input type="text" name="email" required />
                         </div>
                         <div className="input-field">
                             <label>Message</label>
-                            <textarea name="message"></textarea>
+                            <textarea name="message" required></textarea>
                         </div>
-                        <button type="submit">Send message</button>
+                        <button type="submit">SEND</button>
                     </form>
                 </div>
             </div>
