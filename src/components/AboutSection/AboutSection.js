1276bde96ad48db4cc8e2e529ea47d798875571e
Caleb-Rogers <rogerscaleb2019@gmail.com>
2023-11-17 05:47:47 -0500

    About Section Redesign
    Buttons and some layout still needed.

--- src/components/AboutSection/AboutSection.js
+++ src/components/AboutSection/AboutSection.js
@@ -1,40 +1,43 @@
 import React from "react";
+import Link from "next/link";
 
 export default function AboutSection() {
   return (
-    <div class="about-container">
-      <div class="about-header">
-        <h1>About Barbara Monroe</h1>
-        <p>Helping designers and engineers for 20 years to get through the legal process of getting
-            their construction project approved, as well as offering expertise on
-            site plans and project management.</p>
-      </div>
-      <div class="about-main-wrapper">
-        <div class="about-main">
-          <div class="about-main-left">
-            <img src="headshot.png" alt="B. Monroe Headshot" />
+    <section className="about-section">
+      <div className="about-sec-container">
+        <div className="about-sec-content">
+          <div className="about-sec-heading">
+            <h2>About Barbara Monroe: Mastering Construction Project Success</h2>
           </div>
-          <div class="about-main-right">
-            <h2>Handling construction logistics so you can focus on what's important</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris aliquet justo quis cursus
-              dignissim. Sed accumsan aliquet urna, quis gravida dolor sagittis in. Proin tincidunt massa
-              maximus consequat congue. Nullam maximus, purus at ultricies facilisis, nisl leo elementum lacus,
-              nec maximus dui felis id velit. Fusce commodo mattis eros id sagittis. Pellentesque congue nulla
-              non urna pharetra, eu ornare urna viverra. Integer sodales consectetur urna, sit amet lacinia eros fringilla nec.
+          <div className="about-sec-sub-heading">
+            <h3>Zoning & Code Enforcement Officer | Construction PM Consultant | Site Plan Designer</h3>
+          </div>
+          <div className="about-sec-description">
+            <p>
+              With a track record of steering projects from concept to completion, I, Barbara Monroe,
+              blend expertise with passion. My commitment is simple: transform your visions into construction
+              triumphs through strategic project management.
             </p>
-            <div class="about-icons">
-              <div class="left-icon">
-                <img src="code-enforcement-logo.png" alt="NYS Licensed Code Enforcement Official LOGO" />
-                <span>NYS Licensed Code Enforcement Official</span>
-              </div>
-              <div class="right-icon">
-                <img src="BPI-logo.png" alt="Building Performance Institute, Inc. IDL Certification LOGO" />
-                <span>Building Performance Institute, Inc. IDL Certification</span>
-              </div>
-            </div>
+          </div>
+          <div className="about-cta-btn">
+            <Link href="/about" className="about-btn">
+                Learn More About Me
+              </Link>
           </div>
         </div>
+        <div className="about-sec-img-box">
+          <div className="about-sec-img">
+            <img src="headshot.png" alt="B. Monroe Headshot" />
+          </div>
+          <div className="icons">
+            <img src="code-enforcement-logo.png" alt="NYS Licensed Code Enforcement Official LOGO" />
+          <span>NYS Licensed Code Enforcement Official</span>
+            <img src="BPI-logo.png" alt="Building Performance Institute, Inc. IDL Certification LOGO" />
+            <span>Building Performance Institute, Inc. IDL Certification</span>
+        </div>
+        </div>
       </div>
-    </div>
+
+    </section>
   );
 };
