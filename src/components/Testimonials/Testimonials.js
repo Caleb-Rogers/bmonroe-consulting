6b46834cae8c7170e9ddec99196a1cf1f1cf5b17
Caleb-Rogers <rogerscaleb2019@gmail.com>
2023-11-27 04:43:55 -0500

    Sizing

--- src/components/Testimonials/Testimonials.js
+++ src/components/Testimonials/Testimonials.js
@@ -28,13 +28,14 @@ export default function Testimonials() {
           <div className="test-col">
             <div className="test-content">
               <p>
-                We highly recommend Barb Monroe and Monroe Consulting to anyone to build a new structure, renovate an existing structure,
-                or help coordinating projects and navigating the permitting and codes process. Barb's expertise is exceptional, and is
-                responsive to needs of her customers. Barb Monroe helped us through a challenging issue and her professionalism and focus
-                was what helped us get through it.
+                Barb Monroe and Monroe Consulting were instrumental in the success of our building project. From the initial planning stages to 
+                navigating the intricacies of permits and codes, Barb's expertise was evident every step of the way. She provided invaluable 
+                insights, kept the project on track, and demonstrated a commitment to excellence that is truly commendable. Barb's professionalism 
+                and dedication made a significant impact, and we wholeheartedly recommend her services to anyone in need of a construction project 
+                management consultant.
               </p>
-              <h3>Joe & Pam Stagliano</h3>
-              <p>Stagliano Inc.</p>
+              <h3>First Last</h3>
+              <p>Random, LLC.</p>
             </div>
             <div className="test-row">
               <div className="test-img">
@@ -45,13 +46,32 @@ export default function Testimonials() {
           <div className="test-col">
             <div className="test-content">
               <p>
-                We highly recommend Barb Monroe and Monroe Consulting to anyone to build a new structure, renovate an existing structure,
-                or help coordinating projects and navigating the permitting and codes process. Barb's expertise is exceptional, and is
-                responsive to needs of her customers. Barb Monroe helped us through a challenging issue and her professionalism and focus
-                was what helped us get through it.
+                We had the pleasure of working with Barb Monroe on our recent construction project, and we cannot recommend her highly enough. 
+                Barb's knowledge of construction project management is unmatched, and her ability to navigate the complexities of permits and 
+                codes made the entire process seamless. Her attention to detail, responsiveness, and professionalism were instrumental in the 
+                success of our project. Barb Monroe truly goes above and beyond to ensure her clients' needs are met, and we are grateful for 
+                her expertise.
               </p>
-              <h3>Joe & Pam Stagliano</h3>
-              <p>Stagliano Inc.</p>
+              <h3>First Last</h3>
+              <p>Random, LLC.</p>
+            </div>
+            <div className="test-row">
+              <div className="test-img">
+                <img src="test-1-img.svg" alt="First Testimonial Image" />
+              </div>
+            </div>
+          </div>
+          <div className="test-col">
+            <div className="test-content">
+              <p>
+                Choosing Barb Monroe for our construction project was one of the best decisions we made. Barb's extensive knowledge of project management, 
+                coupled with her attention to detail, proved to be invaluable throughout the entire process. She skillfully guided us through the complexities 
+                of permits and codes, ensuring a smooth and efficient project timeline. Barb's commitment to client satisfaction, coupled with her unwavering 
+                professionalism, made the entire experience positive and stress-free. We are grateful for Barb Monroe's expertise and highly recommend her 
+                services to anyone in need of a top-notch construction project management consultant.
+              </p>
+              <h3>First Last</h3>
+              <p>Random, LLC.</p>
             </div>
             <div className="test-row">
               <div className="test-img">
