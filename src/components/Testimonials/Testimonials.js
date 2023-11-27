cf8890c0aa7f4fbfc207e7c944f9ff1d975c979f
Caleb-Rogers <rogerscaleb2019@gmail.com>
2023-11-27 04:29:58 -0500

    Added Row of Testimonial Cards
    Need to convert into carousel

--- src/components/Testimonials/Testimonials.js
+++ src/components/Testimonials/Testimonials.js
@@ -1,81 +1,66 @@
 import React from "react";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-function Testimonials() {
-  return (
-    <div className="testimonials">
-      <Container className="pt-4">
-        <h1 className="text-center section-heading-blue pt-4">Testimonials</h1>
-        <hr></hr>
-        <Row md={3}>
-          <Col>
-            <div className="container">
-              <blockquote className="quote-box">
-                <p className="quotation-mark px-5">“</p>
-                <p className="quote-text px-5">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                  eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                  enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                  nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                  in reprehenderit in voluptate velit es
-                </p>
-                <hr></hr>
-                <div className="blog-post-actions">
-                  <p className="blog-post-bottom pull-left">Abraham Lincoln</p>
-                  <p className="blog-post-bottom pull-right">
-                    <span className="badge quote-badge">743</span>  
-                  </p>
-                </div>
-              </blockquote>
-            </div>
-          </Col>
-          <Col>
-            <div className="container">
-              <blockquote className="quote-box">
-                <p className="quotation-mark px-5">“</p>
-                <p className="quote-text px-5">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                  eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                  enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                  nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                  in reprehenderit in voluptate velit es
-                </p>
-                <hr></hr>
-                <div className="blog-post-actions">
-                  <p className="blog-post-bottom pull-left">Abraham Lincoln</p>
-                  <p className="blog-post-bottom pull-right">
-                    <span className="badge quote-badge">743</span>  
-                  </p>
-                </div>
-              </blockquote>
-            </div>
-          </Col>
-          <Col>
-            <div className="container">
-              <blockquote className="quote-box">
-                <p className="quotation-mark px-5">“</p>
-                <p className="quote-text px-5">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                  eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                  enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                  nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                  in reprehenderit in voluptate velit es
-                </p>
-                <hr></hr>
-                <div className="blog-post-actions">
-                  <p className="blog-post-bottom pull-left">Abraham Lincoln</p>
-                  <p className="blog-post-bottom pull-right">
-                    <span className="badge quote-badge">743</span>  
-                  </p>
-                </div>
-              </blockquote>
-            </div>
-          </Col>
-        </Row>
-      </Container>
-    </div>
-  );
-}
 
-export default Testimonials;
+export default function Testimonials() {
+  return (
+    <section className="test-sec">
+      <div className="test-heading">
+        <h2 className="test-heading">Testimonials: What Past Clients Have Said</h2>
+      </div>
+      <div className="test-container">
+        <div className="test-row">
+          <div className="test-col">
+            <div className="test-content">
+              <p>
+                We highly recommend Barb Monroe and Monroe Consulting to anyone to build a new structure, renovate an existing structure,
+                or help coordinating projects and navigating the permitting and codes process. Barb's expertise is exceptional, and is
+                responsive to needs of her customers. Barb Monroe helped us through a challenging issue and her professionalism and focus
+                was what helped us get through it.
+              </p>
+              <h3>Joe & Pam Stagliano</h3>
+              <p>Stagliano Inc.</p>
+            </div>
+            <div className="test-row">
+              <div className="test-img">
+                <img src="test-1-img.svg" alt="First Testimonial Image" />
+              </div>
+            </div>
+          </div>
+          <div className="test-col">
+            <div className="test-content">
+              <p>
+                We highly recommend Barb Monroe and Monroe Consulting to anyone to build a new structure, renovate an existing structure,
+                or help coordinating projects and navigating the permitting and codes process. Barb's expertise is exceptional, and is
+                responsive to needs of her customers. Barb Monroe helped us through a challenging issue and her professionalism and focus
+                was what helped us get through it.
+              </p>
+              <h3>Joe & Pam Stagliano</h3>
+              <p>Stagliano Inc.</p>
+            </div>
+            <div className="test-row">
+              <div className="test-img">
+                <img src="test-1-img.svg" alt="First Testimonial Image" />
+              </div>
+            </div>
+          </div>
+          <div className="test-col">
+            <div className="test-content">
+              <p>
+                We highly recommend Barb Monroe and Monroe Consulting to anyone to build a new structure, renovate an existing structure,
+                or help coordinating projects and navigating the permitting and codes process. Barb's expertise is exceptional, and is
+                responsive to needs of her customers. Barb Monroe helped us through a challenging issue and her professionalism and focus
+                was what helped us get through it.
+              </p>
+              <h3>Joe & Pam Stagliano</h3>
+              <p>Stagliano Inc.</p>
+            </div>
+            <div className="test-row">
+              <div className="test-img">
+                <img src="test-1-img.svg" alt="First Testimonial Image" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
