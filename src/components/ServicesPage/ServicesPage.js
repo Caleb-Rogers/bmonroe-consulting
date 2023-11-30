dd3f7ffe518d3676ac3678646034994ee3599bb4
Caleb-Rogers <rogerscaleb2019@gmail.com>
2023-11-30 14:46:15 -0500

    Services Page Responsiveness

--- src/components/ServicesPage/ServicesPage.js
+++ src/components/ServicesPage/ServicesPage.js
@@ -132,17 +132,19 @@ export default function Service() {
                             <li>Managing properties for absentee landowners</li>
                         </ul>
                         <br></br>
-                    </div>s
+                    </div>
                 </div>
                 <div className="service-main-right">
                     <img className="headshot" src="headshot.png" alt="B. Monroe Headshot" />
-                    <h4>Barbara Monroe</h4>
-                    <h5>Founder at <Link className="founder" href="/">B. MONROE CONSULTING</Link></h5>
+                    <div className="client-text">
+                      <h4>Barbara Monroe</h4>
+                      <h5>Founder at <Link className="founder" href="/">B. MONROE CONSULTING</Link></h5>
+                    </div>
                     <div className="socials">
                       <Link href="https://www.linkedin.com/in/barbara-monroe-6088439a/" target="_blank"><img src="linkedin-500-yellow.png" alt="LinkedIn Icon" /></Link>
                       <Link href="https://www.facebook.com/barb.monroe.18" target="_blank"><img src="facebook-500-yellow.png" alt="Facebook Icon" /></Link>
                     </div>
-                    <Link className="cta" href="/contact">Work With Me</Link>
+                    <Link className="service-pg-cta" href="/contact">Work With Me</Link>
                 </div>
             </div>
         </div>
