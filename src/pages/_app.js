9c5e29fecbff3c9c41e830b27c0dab9c531c6283
Caleb-Rogers <rogerscaleb2019@gmail.com>
2024-01-08 17:46:06 -0500

    Bootstrap Import Reorganization

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -12,22 +12,15 @@ import "../components/AboutPage/AboutPage.css";
 import "../components/ServicesPage/ServicesPage.css";
 import "../components/Contact/ContactPage.css";
 
-<link rel="stylesheet" href="assets/css/style.css" />;
-
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.css"
-          integrity="sha512-UTNP5BXLIptsaj5WdKFrkFov94lDx+eBvbKyoe1YAfjeRPC+gT5kyZ10kOHCfNZqEui1sxmqvodNUx3KbuYI/A=="
-          crossorigin="anonymous" referrerpolicy="no-referrer" />
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
-          integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
-          crossorigin="anonymous" referrerpolicy="no-referrer" />
       </Head>
       <Component {...pageProps} />
     </>
   );
 }
+
 export default MyApp;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,4 @@
 import Homepage from "../components/Homepage";
-import "bootstrap/dist/css/bootstrap.min.css";
-<link
-  rel="stylesheet"
-  href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
-  integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
-  crossorigin="anonymous"
-/>;
 
 export default function Home() {
   return <Homepage />;
