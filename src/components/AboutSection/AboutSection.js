dda56937695981e881948b026673d6a186a80516
Caleb-Rogers <rogerscaleb2019@gmail.com>
2024-01-17 00:37:00 -0500

    Refactoring but Same Results
    Refactored "about-overlay" with some HTML structure and corresponding CSS changes, which accomplished the same look but 'overfill: hidden' still isn't working.

--- src/components/AboutSection/AboutSection.js
+++ src/components/AboutSection/AboutSection.js
@@ -34,48 +34,46 @@ export default function AboutSection() {
   }, []);
 
   return (
-    <>
+    <section className="about-section">
       <div id="about-overlay" className="about-overlay"></div>
-      <section className="about-section">
-        <div className="about-sec-container">
-          <div className="about-sec-text" id="about-sec-text">
-            <div className="about-sec-line"></div>
-            <h2 className="about-sec-sub-heading">About Barbara Monroe</h2>
-            <h1 className="about-sec-heading">Mastering Construction<br></br>Project Success</h1>
-            <h3 className="about-sec-titles"><span>Zoning & Code Enforcement Officer</span><span className="dot"> • </span><span>Construction PM Consultant</span><span className="dot"> • </span><span>Site Plan Designer</span></h3>
-            <p className="about-sec-description">
-              With a track record of steering projects over obstacles to approval,
-              I, Barbara Monroe, blend expertise with passion. My commitment is simple:
-              transform your visions into construction triumphs through strategic
-              project management.
-            </p>
-          </div>
-          <div className="about-sec-cta">
-            <div className="about-sec-icons">
-              <div className="about-sec-icon-group">
-                <a href="https://dos.ny.gov/building-standards-and-codes/" target="_blank">
-                  <img src="code-enforcement-logo.png" className="about-sec-icon" alt="NYS Licensed Code Enforcement Official LOGO" />
-                  <span className="about-sec-icon-text">NYS Licensed Code Enforcement Official</span>
-                </a>
-              </div>
-              <div className="about-sec-icon-group">
-                <a href="https://www.bpi.org/certified-professionals/infiltration-duct-leakage" target="_blank">
-                  <img src="BPI-logo.png" className="about-sec-icon" alt="Building Performance Institute, Inc. IDL Certification LOGO" />
-                  <span className="about-sec-icon-text">Building Performance Institute, Inc. IDL Certification</span>
-                </a>
-              </div>
+      <div className="about-sec-container">
+        <div className="about-sec-text" id="about-sec-text">
+          <div className="about-sec-line"></div>
+          <h2 className="about-sec-sub-heading">About Barbara Monroe</h2>
+          <h1 className="about-sec-heading">Mastering Construction<br></br>Project Success</h1>
+          <h3 className="about-sec-titles"><span>Zoning & Code Enforcement Officer</span><span className="dot"> • </span><span>Construction PM Consultant</span><span className="dot"> • </span><span>Site Plan Designer</span></h3>
+          <p className="about-sec-description">
+            With a track record of steering projects over obstacles to approval,
+            I, Barbara Monroe, blend expertise with passion. My commitment is simple:
+            transform your visions into construction triumphs through strategic
+            project management.
+          </p>
+        </div>
+        <div className="about-sec-cta">
+          <div className="about-sec-icons">
+            <div className="about-sec-icon-group">
+              <a href="https://dos.ny.gov/building-standards-and-codes/" target="_blank">
+                <img src="code-enforcement-logo.png" className="about-sec-icon" alt="NYS Licensed Code Enforcement Official LOGO" />
+                <span className="about-sec-icon-text">NYS Licensed Code Enforcement Official</span>
+              </a>
             </div>
-            <div className="about-sec-btn">
-              <Link href="/about" className="tertiary-btn">
-                <span>Learn More About Me</span>
-              </Link>
+            <div className="about-sec-icon-group">
+              <a href="https://www.bpi.org/certified-professionals/infiltration-duct-leakage" target="_blank">
+                <img src="BPI-logo.png" className="about-sec-icon" alt="Building Performance Institute, Inc. IDL Certification LOGO" />
+                <span className="about-sec-icon-text">Building Performance Institute, Inc. IDL Certification</span>
+              </a>
             </div>
           </div>
+          <div className="about-sec-btn">
+            <Link href="/about" className="tertiary-btn">
+              <span>Learn More About Me</span>
+            </Link>
+          </div>
         </div>
-        <div className="about-sec-img-box" id="about-sec-img-box">
-          <img src="headshot.png" className="about-sec-img" alt="B. Monroe Headshot" />
-        </div>
-      </section>
-    </>
+      </div>
+      <div className="about-sec-img-box" id="about-sec-img-box">
+        <img src="headshot.png" className="about-sec-img" alt="B. Monroe Headshot" />
+      </div>
+    </section>
   );
 };
