5ababb92274b29f3bece76f030e980446755f072
Caleb-Rogers <rogerscaleb2019@gmail.com>
2024-01-18 23:24:17 -0500

    About Page Classes

--- src/components/AboutPage/AboutPage.js
+++ src/components/AboutPage/AboutPage.js
@@ -35,12 +35,12 @@ export default function About() {
         <div className="about-container">
             <div className="about-main">
                 <div className="about-main-left">
-                    <p>Hello 👋</p>
+                    <p className="about-text">Hello 👋</p>
                     <br></br>
-                    <h1>I’m Barbara Monroe, founder of B. Monroe Consulting. I offer zoning and project management services
+                    <h1 className="about-heading">I’m Barbara Monroe, founder of B. Monroe Consulting. I offer zoning and project management services
                         that create happy clients through meticulous and forward-thinking plans and execution.</h1>
                     <br></br>
-                    <p>I have over two decades of experience as a zoning enforcement officer. As of recent years, I have
+                    <p className="about-text">I have over two decades of experience as a zoning enforcement officer. As of recent years, I have
                         transitioned and adapted to consulting contractors on their construction projects. With my acquired
                         experience, I can create site plans, dispute variance issues, and aid you through the tedious legal
                         process to getting your project APPROVED.
@@ -49,10 +49,10 @@ export default function About() {
                     <br></br>
                     <br></br>
                     <div className="hidden">
-                        <h2>WORK THAT I'M PROUD OF</h2>
+                        <h2 className="about-sub-heading">WORK THAT I'M PROUD OF</h2>
                         <br></br>
-                        <p>Mostly:</p>
-                        <p>I was the lead project manager for <a href="https://www.redpointny.com/" target="_blank">Red Point</a>. During my
+                        <p className="about-text">Mostly:</p>
+                        <p className="about-text">I was the lead project manager for <a href="https://www.redpointny.com/" target="_blank">Red Point</a>. During my
                             time in that role I designed site plans and processed permits to get approved, allowing the business
                             to execute on their construction project. I cultivated meaningful relationships with with my clients
                             and am regarded as a efficient and effective member that can improve any team I join.
@@ -60,9 +60,9 @@ export default function About() {
                         <br></br>
                     </div>
                     <div className="hidden">
-                        <p>But also:</p>
-                        <ul>
-                            <li>Built and structured the Construction Project Manager Consultant role from the ground up</li>
+                        <p className="about-text">But also:</p>
+                        <ul className="about-list">
+                            <li className="about-list-item">Built and structured the Construction Project Manager Consultant role from the ground up</li>
                             <li>Outlined preliminary structure for facilitating streamlined and efficient communication between internal teams</li>
                             <li>Shifted primary focus of ongoing partnerships from a task based structure to strategy based structure</li>
                             <li>Created customized strategic planning recommendations matching clients exact needs</li>
@@ -76,64 +76,64 @@ export default function About() {
                         <br></br>
                     </div>
                     <div className="hidden">
-                        <h2>FROM THE CAREER ARCHIVES</h2>
+                        <h2 className="about-sub-heading">FROM THE CAREER ARCHIVES</h2>
                         <br></br>
-                        <h3>Construction Project Management Consultant, Red Point | 2019 - 2023</h3>
-                        <p>Maintained, developed, increased client partnerships by cultivating close relationships, built on trust and shared vision.</p>
+                        <h3 className="about-title">Construction Project Management Consultant, Red Point | 2019 - 2023</h3>
+                        <p className="about-text">Maintained, developed, increased client partnerships by cultivating close relationships, built on trust and shared vision.</p>
                         <br></br>
-                        <h3>Site Plan Designer, Other Company | 2018-2022</h3>
-                        <p>Managed multiple large scale redesign projects (averaging 12 months) and general ongoing support clients simultaneously</p>
-                        <br></br>
-                        <br></br>
-                        <br></br>
-                    </div>
-                    <div className="hidden">
-                        <h2>NOTEWORTHY CLIENT RELATIONSHIPS AS CONSTRUCTION PROJECT MANAGER CONSULTANT</h2>
-                        <br></br>
-                        <div><a href="https://www.redpointny.com/" target="_blank">Client 1</a></div>
-                        <div><a href="https://www.redpointny.com/" target="_blank">Client 2</a></div>
-                        <div><a href="https://www.redpointny.com/" target="_blank">Client 3</a></div>
-                        <div><a href="https://www.redpointny.com/" target="_blank">Client 4</a></div>
-                        <div><a href="https://www.redpointny.com/" target="_blank">Client 5</a></div>
+                        <h3 className="about-title">Site Plan Designer, Other Company | 2018-2022</h3>
+                        <p className="about-text">Managed multiple large scale redesign projects (averaging 12 months) and general ongoing support clients simultaneously</p>
                         <br></br>
                         <br></br>
                         <br></br>
                     </div>
                     <div className="hidden">
-                        <h2>PROFESSIONAL DEVELOPMENT</h2>
+                        <h2 className="about-sub-heading">NOTEWORTHY CLIENT RELATIONSHIPS AS CONSTRUCTION PROJECT MANAGER CONSULTANT</h2>
                         <br></br>
-                        <h3>Positioning Your Agency Workshop</h3>
-                        <p>Bureau of Digital | September 2022</p>
-                        <br></br>
-                        <h3>Biz Dev Workshop</h3>
-                        <p>Bureau of Digital | June 2022</p>
-                        <br></br>
-                        <h3>Account Management Camp</h3>
-                        <p>Bureau of Digital | May 2022</p>
-                        <br></br>
-                        <h3>Account Management Camp</h3>
-                        <p>Bureau of Digital | May 2021</p>
+                        <div><a className="about-link" href="https://www.redpointny.com/" target="_blank">Client 1</a></div>
+                        <div><a className="about-link" href="https://www.redpointny.com/" target="_blank">Client 2</a></div>
+                        <div><a className="about-link" href="https://www.redpointny.com/" target="_blank">Client 3</a></div>
+                        <div><a className="about-link" href="https://www.redpointny.com/" target="_blank">Client 4</a></div>
+                        <div><a className="about-link" href="https://www.redpointny.com/" target="_blank">Client 5</a></div>
                         <br></br>
                         <br></br>
                         <br></br>
                     </div>
                     <div className="hidden">
-                        <h2>ABOUT ME</h2>
+                        <h2 className="about-sub-heading">PROFESSIONAL DEVELOPMENT</h2>
                         <br></br>
-                        <h3>Family movie nights</h3>
-                        <h3>Draft Beer</h3>
-                        <h3>Dinner and drinks</h3>
-                        <h3>Amy Schumer</h3>
-                        <h3>Daptone Records</h3>
-                        <h3>Pasta</h3>
-                        <h3>Sunday hangs</h3>
+                        <h3 className="about-title">Positioning Your Agency Workshop</h3>
+                        <p className="about-text">Bureau of Digital | September 2022</p>
+                        <br></br>
+                        <h3 className="about-title">Biz Dev Workshop</h3>
+                        <p className="about-text">Bureau of Digital | June 2022</p>
+                        <br></br>
+                        <h3 className="about-title">Account Management Camp</h3>
+                        <p className="about-text">Bureau of Digital | May 2022</p>
+                        <br></br>
+                        <h3 className="about-title">Account Management Camp</h3>
+                        <p className="about-text">Bureau of Digital | May 2021</p>
+                        <br></br>
+                        <br></br>
+                        <br></br>
+                    </div>
+                    <div className="hidden">
+                        <h2 className="about-sub-heading">ABOUT ME</h2>
+                        <br></br>
+                        <h3 className="about-title">Family movie nights</h3>
+                        <h3 className="about-title">Draft Beer</h3>
+                        <h3 className="about-title">Dinner and drinks</h3>
+                        <h3 className="about-title">Amy Schumer</h3>
+                        <h3 className="about-title">Daptone Records</h3>
+                        <h3 className="about-title">Pasta</h3>
+                        <h3 className="about-title">Sunday hangs</h3>
                     </div>
                 </div>
                 <div className="about-main-right">
                     <img className="headshot" src="headshot.png" alt="B. Monroe Headshot" />
                     <div className="client-text">
-                      <h4>Barbara Monroe</h4>
-                      <h5>Founder at <Link className="founder" href="/">B. MONROE CONSULTING</Link></h5>
+                      <h4 className="about-name">Barbara Monroe</h4>
+                      <h5 className="about-info">Founder at <Link className="founder" href="/">B. MONROE CONSULTING</Link></h5>
                     </div>
                     <Link className="about-pg-cta" href="/contact">Work With Me</Link>
                 </div>
